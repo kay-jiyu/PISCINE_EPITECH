3b3c8fb99d87b1cf7c30dc912496812fa49f3def
自由Kay <venancekayode86@gmail.com>
2026-02-11 22:10:55 +0100

    Update my_putstr.c

--- CPOOL_Day04_ACADEMIC2026/my_putstr.c
+++ CPOOL_Day04_ACADEMIC2026/my_putstr.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
 
-void my_putchar(char a)
-{
-    write(1, &a, 1);
-}
 
 int my_putstr(char const *str)
 {
@@ -17,12 +11,4 @@ int my_putstr(char const *str)
     }
 }
 
-int main(void)
-{
 
-    char const *string = "coding";
-    my_putstr(string);
-    my_putchar('\n');
-
-    return 0;
-}
