b352ffd35d4a2c6dcef11ec01339d35dac7a7d81
自由Kay <venancekayode86@gmail.com>
2026-03-19 00:37:11 +0100

    Create my_print_params.c

---
+++ CPOOL_Day07_ACADEMIC2026/task04/my_print_params.c
@@ -0,0 +1,15 @@
+#include "my.h"
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+
+    while (i < argc)
+    {
+        my_putstr(argv[i]);
+        my_putchar('\n');
+        i++;
+    }
+    return 0;
+}
